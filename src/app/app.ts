3ab68b828bec4f980a66a81a7ce4aa7acd00ee79
Exp <17885980+exp111@users.noreply.github.com>
2025-11-05 21:00:58 +0100

    test players

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface Player {
+  name: string;
+  score: number;
+  color: string;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -8,4 +14,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+  players = [
+    {
+      name: "Jane",
+      score: 160,
+      color: "#9183A5"
+    },
+    {
+      name: "Tim",
+      score: 60,
+      color: "#585A65"
+    }
+  ];
 }
